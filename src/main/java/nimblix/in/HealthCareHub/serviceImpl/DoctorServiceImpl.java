a128e108a30d6045956b600a68d6b9df5c2e9592
nithinrajkumar559-blip <nithinrajkumarnimblix@outlook.com>
2026-03-11 10:09:37 +0530

    fix: resolved all conflicts in DoctorService, DoctorController, DoctorServiceImpl

--- src/main/java/nimblix/in/HealthCareHub/serviceImpl/DoctorServiceImpl.java
+++ src/main/java/nimblix/in/HealthCareHub/serviceImpl/DoctorServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
